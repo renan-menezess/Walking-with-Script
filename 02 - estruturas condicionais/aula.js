08201e309ec7a05e54a23fbff192b8226a62c0d0
Renan Menezes <2003rcsm@gmail.com>
2023-11-23 15:17:48 -0300

    If e Else

--- 02 - estruturas condicionais/aula.js
+++ 02 - estruturas condicionais/aula.js
@@ -1,6 +1,11 @@
 
-const numero = 10;
+const numero = 3;
 
-const numeroPar = numero % 2;
+const numeroPar = (numero % 2) === 0;
 
-console.log(numeroPar);
+if (numeroPar) {
+    console.log('Par');
+} else {
+    console.log('Impar');
+}
+
