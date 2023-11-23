45d2217bdc8b8c9ab917553afb4797fe9b6589f2
Renan Menezes <2003rcsm@gmail.com>
2023-11-23 16:30:54 -0300

    Exemplo com function

--- 04 - funcoes/aula.js
+++ 04 - funcoes/aula.js
@@ -1,10 +1,25 @@
 
-
-function incrementarJuros(valor, percentualJuros) {
-    const valosDeAcrescimo = (percentualJuros / 100) * valor;
-    return valor +valosDeAcrescimo
+function calcularImc(peso, altura) {
+    return peso / Math.pow(altura, 2);
 }
 
-console.log(incrementarJuros (100, 10));
-console.log(incrementarJuros (100, 15));
-console.log(incrementarJuros (100, 20));
+function classificarImc(imc) {
+    if (imc < 18.5) {
+        return 'Abaixo do peso';
+    } else if (imc >= 18.5 && imc , 25){
+        return 'Peso normal';
+    } else if (imc >= 25 && imc , 30){
+        return 'Acima do peso';
+    } else if (imc >= 30 && imc , 40){
+        return 'Obeso';
+    } else {
+        return 'Obsedidade grave'
+    }
+}
+
+const peso = 75;
+const altura = 1.75;
+const imc = calcularImc(peso, altura);
+console.log(imc);
+
+
